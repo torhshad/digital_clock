fafabba94fec94c11448fa96c86d52816fb1a15d
shadtorh <torhshad09@gmail.com>
2025-01-26 17:21:51 -0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-let errorOnLoad = alert(
-	"Error: The page you are trying to access is not available."
-);
+function updateClock() {
+	const clock = document.getElementById("clock");
+	const now = new Date();
+	const hours = String(now.getHours()).padStart(2, "0");
+	const minutes = String(now.getMinutes()).padStart(2, "0");
+	const seconds = String(now.getSeconds()).padStart(2, "0");
+
+	clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
